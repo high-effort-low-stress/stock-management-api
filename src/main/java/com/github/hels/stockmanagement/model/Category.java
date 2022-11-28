3d9d10dce4a43f183f3282734ca781f8bd679978
Caio Torres <caiocftorres@gmail.com>
2022-11-28 20:00:14 -0300

    fix: entities relationship

--- src/main/java/com/github/hels/stockmanagement/model/Category.java
+++ src/main/java/com/github/hels/stockmanagement/model/Category.java
@@ -22,7 +22,8 @@ public class Category {
     @ManyToOne
     @JoinColumn(name = "parent_id")
     private Category parent;
-    @ManyToMany
-    @JoinColumn(name = "product_id")
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products;
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children;
 }
